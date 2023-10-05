7fd9a603615035db4b3055e9a415043d84d5f437
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-06 00:05:49 +0300

    print out a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *Return: returns 1 if success
+ */
+int main(void)
+{
+	puts("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
