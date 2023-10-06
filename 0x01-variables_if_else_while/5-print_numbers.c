a054a7ec28afd528ad7f184e6bcd8a0233ed269e
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-06 23:24:56 +0300

    prints from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - execution starts from here
+ * Return: 0 ends the program
+ * prints single digit number of base 10
+ */
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
