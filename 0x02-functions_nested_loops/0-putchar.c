b66fbd29f673060269ddf41a09d9369140e0a4fc
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-11 21:39:15 +0300

    prints using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - entry point to the program
  * Return: 0 and finishes the program
  */
 int main(void)
 {
+	char word[] = "_putchar\n";
+	int char_counter = 0;
+
+	while (char_counter < 9)
+	{
+		_putchar(word[char_counter]);
+		char_counter++;
+	}
 	printf("_putchar\n");
 	return (0);
 }
