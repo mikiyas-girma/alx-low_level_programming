698b7b99ac5148368e7bf47c066890c3ab91bde4
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-11 22:46:25 +0300

    shit gcc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - entry point
+ * print_alphabet - displays lowercase letters
  * Return: exit when finishes
  */
 void print_alphabet(void)
 {
 	int lowers = 'a';
 
-	while(lowers <= 'z')
+	while (lowers <= 'z')
 	{
 		_putchar(lowers);
 		lowers++;
