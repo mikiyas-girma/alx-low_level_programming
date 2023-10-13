a51e6bd990f60d8e91eae7ad8afe8319ab91806e
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-13 21:25:54 +0300

    print single digit numbers using putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_numbers - print numbers from 0 to 9
+ * @i: ascii value from 0 -9
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i <=57; i++)
+		putchar(i);
+	putchar('\n');
+}
