4d3d5d310bbf4109b1245e9ae64b1b3bc8a1b5eb
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-13 16:02:12 +0300

    isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks a letter
+ * @c: character
+ * Return: 0 or 1 depending on the condition
+ */
+int _isupper(int c)
+{
+	if (isupper(c) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
