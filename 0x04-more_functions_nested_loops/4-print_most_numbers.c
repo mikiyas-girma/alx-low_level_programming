0a40110e711e58532c66b7af17460ca7bafb1761
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2023-10-13 23:20:53 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,17 @@
  * print_most_numbers - prints from 0 to 9 except from 2 and 4 using putchar
  * @num: variable for the numbers
  */
+
 void print_most_numbers(void)
 {
-	char num = '0';
+	char num;
 
-	while (num < '10')
+	for (num = '0'; num <= '9'; num++)
 	{
 		if (num != '2'  && num != '4')
 		{
 			_putchar(num);
 		}
-		num++;
 	}
 	_putchar('\n');
 }
