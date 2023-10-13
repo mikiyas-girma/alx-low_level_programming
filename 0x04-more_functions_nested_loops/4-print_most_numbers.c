758510d484fab7e1f04cd7588b51185d544d6640
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-13 21:45:25 +0300

    prints numbers from 0-9 except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints from 0 to 9 except from 2 and 4 using putchar
+ * @num: variable for the numbers
+ */
+void print_most_numbers(void)
+{
+	int num = 48;
+
+	while(num <= 58)
+	{
+		if (num != 50 && num != 52)
+		{
+			putchar(num);
+		}
+		num++;
+		putchar('\n');
+	}
+}
