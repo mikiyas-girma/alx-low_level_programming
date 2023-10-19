47139bfd76dcda0a8b46e32598c7f1d870f80477
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-19 20:37:44 +0300

    make betty styled

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,12 @@
 #include <string.h>
 /**
  * *_strcat - concatenates two strings
+ * @dest: destination for concatenated string
+ * @src: src string to be concatenated to
  * Return: the concatenated string
  */
 char *_strcat(char *dest, char *src)
 {
-	return strcat(dest, src);
+	return (strcat(dest, src));
 }
 
