a91df745e54516a10325e28b5353772ef65da322
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-19 21:09:11 +0300

    fixed the undeclared variable issue

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int l;
 
 	l = 0;
-	while (l < n && src[j] != '\0')
+	while (l < n && src[l] != '\0')
 	{
-		dest[j] = src[j];
-		j++;
+		dest[l] = src[l];
+		l++;
 	}
-	while (j < n)
+	while (l < n)
 	{
-		dest[j] = '\0';
-		j++;
+		dest[l] = '\0';
+		l++;
 	}
 
 	return (dest);
