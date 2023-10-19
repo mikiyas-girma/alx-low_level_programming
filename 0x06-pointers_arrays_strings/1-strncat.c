85bc53bd3d2d5ad7d8a3e0bacaf2615d5b0440f7
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-19 20:44:39 +0300

    concatenate n number of chars

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * *_strncat - concatenates n strings form src to dest
+ * @dest: place where concatenated strings stored
+ * @src: source strings to be concatenated to dest
+ * @n: n numbers of char to be concatenated
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
