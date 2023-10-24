c7c4cc4756230814a567c4d62ce886fb6df6f2d2
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-10-24 21:16:19 +0300

    betty style

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,11 +5,11 @@
   * @accept: string containing the list of characters to match in s
   *
   * Return: the number of bytes in the initial segment
-  * of s which consist only of bytes from accept * *_strspn - gets the length of a prefix substring
+  * of s which consist only of bytes from accept
+  *_strspn - gets the length of a prefix substring
   * @s: string to evaluate
   * @accept: string containing the list of characters to match in s
   *
-  * Return: the number of bytes in the initial segment
   * of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int n = 0;
 	int r;
 
-	while(*s)
+	while (*s)
 	{
 		for (r = 0; accept[r]; r++)
 		{
@@ -26,7 +26,7 @@ unsigned int _strspn(char *s, char *accept)
 				n++;
 				break;
 			}
-			else if (accept[r+1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
