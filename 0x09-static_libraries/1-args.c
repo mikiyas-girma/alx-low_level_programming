234e15260f56f8cc603a5182c29797300930deb5
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-29 21:56:37 +0300

    corrected the answer

--- 0x09-static_libraries/1-args.c
+++ 0x09-static_libraries/1-args.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints number of arguments that are passed when running
- * the program but not including program name.
- * @argc: argument count
- * @argv: array of argument strings
- * Return: success (0)
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
