798406588628e63c87e488003cf2a34a9bbb4cdd
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-11-06 22:17:18 +0300

    made consistent with betty style

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - prints the name of the file it was compiled from,
- *   * followed by a new line.
- *    * Return: success(0)
- *     */
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line.
+ * Return: success(0)
+ */
 int main(void)
 {
 		printf("%s\n", __FILE__);
