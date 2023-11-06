5dba4bd2fd2cb69ef84aa4e0bb24a4e6f5800e01
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-11-07 01:13:36 +0300

    structure done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - frees dogs.
+ * @d: a pointer to dogs.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((*d).name);
+		free(d->owner);
+	}
+	free(d);
+}
