97ff92615d389b29733c4e8117453f45262ebb9e
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-11-21 00:37:53 +0300

    betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 /**
- * print_listint - prints all the elements of a listint_t list.
- * @h: pointer to the head of the list.
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the head of the list
  *
- * Return: number of lists.
+ * Return: number of lists
  */
-
-size_t print_listint(const listint_t *head)
+size_t print_listint(const listint_t *h)
 {
   size_t count = 0;
 
