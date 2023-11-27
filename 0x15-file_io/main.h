a4eae7e9fe9d69202cac708044ac3eea1652d033
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-11-27 21:12:40 +0300

    betty styled

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int _putchar(char c);int _putchar(char c);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
