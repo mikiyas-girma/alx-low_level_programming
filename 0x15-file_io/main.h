5652ea1be21e140e3db011e99aa6af31b33721bb
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-11-27 20:48:23 +0300

    added the header file with first prototype

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int _putchar(char c);int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
