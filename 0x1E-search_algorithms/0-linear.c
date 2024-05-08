dc8fbc470a538ae140e70af281df441fe015ff56
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-05-08 11:14:10 +0300

    readable code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,8 +10,10 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+
 	if (!array || size == 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
